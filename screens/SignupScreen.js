8e8a474f450e3eda0b7c4032dc5d63be42f35aeb
Daniil_Khobotov <dinich44@gmail.com>
2023-11-20 18:24:23 +0400

    add login/logout

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,24 +1,28 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Alert } from 'react-native'
 
 import AuthContent from '../components/Auth/AuthContent'
 import { createUser } from '../util/auth'
 import LoadingOverlay from '../components/ui/LoadingOverlay'
+import { AuthContext } from '../store/auth-context'
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
 
+  const authCtx = useContext(AuthContext)
+
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true)
     try {
-      await createUser(email, password)
+      const token = await createUser(email, password)
+      authCtx.authenticate(token)
     } catch (error) {
       Alert.alert(
         'Authenication failed',
         'Could not create user, please check your input and try again later.'
       )
+      setIsAuthenticating(false)
     }
-    setIsAuthenticating(false)
   }
 
   if (isAuthenticating) {
