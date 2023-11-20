7e94140f42192c617ca725d11b7d2b4e4653c632
Daniil_Khobotov <dinich44@gmail.com>
2023-11-20 15:04:54 +0400

    add login

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import AuthContent from '../components/Auth/AuthContent'
 import { createUser } from '../util/auth'
 import LoadingOverlay from '../components/ui/LoadingOverlay'
--- util/auth.js
+++ util/auth.js
@@ -1,12 +1,21 @@
 import axios from 'axios'
 
 const API_KEY = 'AIzaSyDiuHEIBzG4tMBh6J6QnCu9WD860QxwusI'
-const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
 
-export async function createUser(email, password) {
-  const response = await axios.post(URL, {
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`
+
+  const response = await axios.post(url, {
     email,
     password,
     returnSecureToken: true,
   })
+}
+
+export async function createUser(email, password) {
+  await authenticate('signUp', email, password)
+}
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password)
 }
